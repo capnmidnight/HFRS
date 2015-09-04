5c6da2dac54f30660c93bf1ba62186661cec0d78
capnmidnight <sean.mcbeth@gmail.com>
2015-09-03 20:30:03 -0700

    fixed restart script

--- restart.sh
+++ restart.sh
@@ -9,7 +9,5 @@ read -e -p "delete old log file? " -i "N" choice
 if [ $choice != "N" ]; then
     rm ~/.forever/HFRSServer.log
 fi
-cd ..
 git pull
-cd server
 forever --uid "HFRSServer" -a start /usr/local/bin/npm start
