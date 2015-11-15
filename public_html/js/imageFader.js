96969199a6bfb6f03c155092fddc28e5eaac7d8c
Sean T. McBeth <sean.mcbeth@gmail.com>
2015-11-15 15:32:27 -0500

    news styling and downloads page

--- public_html/js/imageFader.js
+++ public_html/js/imageFader.js
@@ -7,10 +7,13 @@
   box.appendChild( disp );
   document.body.appendChild( box );
   for ( var i = 0; i < imgs.length; ++i ) {
-    imgs[i].addEventListener( "click", function ( box, disp ) {
-      box.className = "show";
-      disp.src = this.src;
-    }.bind( imgs[i], box, disp ), false );
+    if(imgs[i].src.indexOf("thumb") >= 0){
+      imgs[i].className = "thumbnail";
+      imgs[i].addEventListener( "click", function ( box, disp ) {
+        box.className = "show";
+        disp.src = this.src.replace("thumb", "full");
+      }.bind( imgs[i], box, disp ), false );
+    }
   }
 
   box.addEventListener( "click", function () {
