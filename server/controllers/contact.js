bc4961cfe3a3b1c905120fcf897fd982ad156582
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-04-07 13:08:35 -0400

    is this going to work?

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -1,17 +1,20 @@
-var fs = require("fs");
+var fs = require("fs"),
+  DATA_FILE_NAME = "contacts_list";
 
 module.exports = {
   pattern: /^\/contacts\/?$/,
   GET: function (params, sendData, serverError, body) {
-    var contacts = fs.readFileSync("contacts", "utf8").trim();
-    if (contacts[contacts.length - 1] === ",") {
-      contacts = contacts.substring(0, contacts.length - 1);
-    }
-    sendData("application/json", "{\"contacts\":[" + contacts + "]}");
+    fs.readFile(DATA_FILE_NAME, "utf8", function (err, contacts) {
+      contacts = contacts.trim();
+      if (contacts[contacts.length - 1] === ",") {
+        contacts = contacts.substring(0, contacts.length - 1);
+      }
+      sendData("application/json", "{\"contacts\":[" + contacts + "]}");
+    });
   },
   POST: function (params, sendData, serverError, body) {
     body.date = new Date();
-    fs.appendFileSync("contacts", JSON.stringify(body) + ",\n");
+    fs.appendFileSync(DATA_FILE_NAME, JSON.stringify(body) + ",\n");
     sendData("application/json", JSON.stringify({
       status: "success",
       message: "success"
