3c08fb223f7b0b92adc1adfbeab2c1cc03a3fb66
Sean T. McBeth <sean.mcbeth@gmail.com>
2016-04-07 16:08:03 -0400

    move it back

--- server/controllers/contact.js
+++ server/controllers/contact.js
@@ -2,7 +2,7 @@ var azure = require("azure-storage"),
   ent = azure.TableUtilities.entityGenerator,
   tables = null;
 
-if (process.env.NODE_ENV === "dev") {
+if (process && process.env && process.env.NODE_ENV === "dev") {
   var secrets = require("./secrets");
   console.log("running DEV database", secrets);
   tables = azure.createTableService(secrets.connectionString);
@@ -10,6 +10,18 @@ if (process.env.NODE_ENV === "dev") {
 else {
   tables = azure.createTableService();
 }
+
+tables.createTableIfNotExists("contacts", function (error, result, response) {
+  if (error) {
+    console.error(error);
+  }
+  else if (result.created) {
+    console.warn("Had to create the contacts table!");
+  }
+  else {
+    console.log("Contacts table already exists.");
+  }
+});
 
 var trans = {
   "name": "PartitionKey",
@@ -38,31 +50,16 @@ module.exports = {
     });
   },
   POST: function (params, sendData, serverError, body) {
-
-
-    tables.createTableIfNotExists("contacts", function (error, result, response) {
-      if (error) {
-        console.error(error);
+    tables.insertOrMergeEntity("contacts", makeEntity(body), function (err, res) {
+      if (err) {
+        console.error("contacts.insertOrMergeEntity:", err);
+        serverError(500);
       }
       else {
-        if (result.created) {
-          console.warn("Had to create the contacts table!");
-        }
-        else {
-          console.log("Contacts table already exists.");
-        }
-        tables.insertOrMergeEntity("contacts", makeEntity(body), function (err, res) {
-          if (err) {
-            console.error("contacts.insertOrMergeEntity:", err);
-            serverError(500);
-          }
-          else {
-            sendData("application/json", JSON.stringify({
-              status: "success",
-              message: res
-            }));
-          }
-        });
+        sendData("application/json", JSON.stringify({
+          status: "success",
+          message: res
+        }));
       }
     });
   }
