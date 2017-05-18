7948927890e47feff11cc01bbe2a9820e2f2532b
Sean T. McBeth <sean.mcbeth@gmail.com>
2017-05-17 20:16:34 -0400

    updaaaates

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,28 @@ var gulp = require("gulp"),
   pkg = require("./package.json"),
   marigold = require("marigold-build").setup(gulp, pkg),
   html = marigold.html(["*.pug"], {
-    watch: ["*.md"],
+    watch: ["*.md", "templates/**/*.pug"],
     data: {
       indexFile: "/index.html",
       headerFiles: ["analytics", "ga", "promise", "requests", "sha512"].map(scriptFile),
       indexFiles: ["contactForm", "imageFader", "mapResizer", "rotator"].map(scriptFile),
     } }),
 
+  css = marigold.css(["css/*.styl"]),
+
   devServer = marigold.devServer(
-    ["src/**/*.js"],
+    [
+      "*.pug",
+      "templates/**/*.pug",
+      "css/**/*.styl",
+      "src/**/*.js"
+    ],
     [
       "!gulpfile.js",
       "*.js",
       "*.css",
+      "css/*.css",
       "*.html"
     ]);
 
-marigold.taskify([html], { default: devServer });
+marigold.taskify([html, css], { default: devServer });
